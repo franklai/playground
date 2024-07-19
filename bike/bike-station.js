765589b4535aee4a831e309e0d70d1439796fefd
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2024-07-20 06:21:12 +0800

    feat: add electric bike count

--- bike/bike-station.js
+++ bike/bike-station.js
@@ -7,7 +7,8 @@ Vue.component("bike-station", {
         <span v-if="station.diff > 30" class="diff-over">t30+</span>
         <span v-else class="diff">t{{ station.diff }}</span>
       </div>
-      <div class="bike">{{ station.bike }}</div>
+      <div class="bike-general">{{ station.general }}</div>
+      <div class="bike-electric">{{ station.electric }}</div>
       <div class="space">{{ station.space }}</div>
     </div>
   `,
